a68587b0386a55f07bbcffd05f944c1eb7f684c8
domagoj.vukovic <off.vukovic@gmail.com>
2022-06-26 01:38:07 +0200

    fix(web): add missing _document page

---
+++ packages/web/src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import { createGetInitialProps } from '@mantine/next'
+import Document, {
+    Html,
+    Main,
+    NextScript,
+} from 'next/document'
+
+const getInitialProps = createGetInitialProps()
+
+export default class _Document extends Document {
+    static getInitialProps = getInitialProps
+
+    render() {
+        return (
+            <Html>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
