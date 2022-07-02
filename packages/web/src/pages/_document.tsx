46714aaec7fc4ccba2eefdb063d771efcbd24d93
domagoj.vukovic <off.vukovic@gmail.com>
2022-07-02 15:29:34 +0200

    feat(web): favicon and site metadata setup

--- packages/web/src/pages/_document.tsx
+++ packages/web/src/pages/_document.tsx
@@ -19,6 +19,36 @@ export default class _Document extends Document {
         return (
             <Html>
                 <Head>
+                    <link
+                        href="/apple-touch-icon.png"
+                        rel="apple-touch-icon"
+                        sizes="180x180"
+                    />
+                    <link
+                        href="/favicon-32x32.png"
+                        rel="icon"
+                        sizes="32x32"
+                        type="image/png"
+                    />
+                    <link
+                        href="/favicon-16x16.png"
+                        rel="icon"
+                        sizes="16x16"
+                        type="image/png"
+                    />
+                    <link
+                        href="/site.webmanifest"
+                        rel="manifest"
+                    />
+                    <link
+                        color="#5bbad5"
+                        href="/safari-pinned-tab.svg"
+                        rel="mask-icon"
+                    />
+                    <meta
+                        content="#ffffff"
+                        name="theme-color"
+                    />
                     <Script
                         src={`https://www.googletagmanager.com/gtag/js?id=${publicRuntimeConfig.GA_TRACKING_ID}`}
                         strategy="afterInteractive"
--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,9 +1,19 @@
 import type { NextPage } from 'next'
+import NextHead from 'next/head'
 
 import { Home } from '../modules'
 
 const HomePage: NextPage = () => {
-    return <Home />
+    return (
+        <>
+            <NextHead>
+                <title>
+                    Blub: Home
+                </title>
+            </NextHead>
+            <Home />
+        </>
+    )
 }
 
 export default HomePage
